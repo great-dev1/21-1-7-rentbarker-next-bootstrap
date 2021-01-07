fb66134c06baa82ce0b73376e1350d8bcbcc41a2
Twenty-August <twentytwentyaugust@outlook.com>
2021-01-07 15:51:15 +0900

    second commit

---
+++ pages/budget/Handle.js
@@ -0,0 +1,29 @@
+export default function Handle({
+  handle: { id, value, percent },
+  getHandleProps,
+  budget,
+}) {
+  return (
+    <div
+      style={{
+        left: `${percent}%`,
+        position: 'absolute',
+        marginLeft: -15,
+        marginTop: 25,
+        zIndex: 2,
+        width: 30,
+        height: 30,
+        border: 0,
+        textAlign: 'center',
+        cursor: 'pointer',
+        borderRadius: '50%',
+        backgroundColor: '#f36b16',
+      }}
+      {...getHandleProps(id)}
+    >
+      <div style={{ width: 90, fontSize: 18, fontWeight: 'bold', marginTop: 45, marginLeft: -30 }}>
+        {budget ? `$${value}` : `${value} months`}
+      </div>
+    </div>
+  )
+}
