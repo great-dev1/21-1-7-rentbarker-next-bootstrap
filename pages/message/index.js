daff8e48257071458f085c887ec9ca2f4f433776
Twenty-August <twentytwentyaugust@outlook.com>
2021-01-11 16:18:23 +0900

    Start of the message page

---
+++ pages/message/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import MyButton from '../../components/MyButton'
+
+export default function Message() {
+  return (
+    <div>
+      <Head>
+        <title>Messages</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main>
+
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
